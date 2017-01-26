213cc204874e1415f0fbf2ed99d98b421f52d046
Caleb Pearce <cpearce31@gmail.com>
2017-01-26 18:58:37 -0500

    Infinite scrolling mostly works. Animation needs work.

--- slides.js
+++ slides.js
@@ -1,39 +1,62 @@
 window.onload = function () {
-  let slideNodes = document.querySelectorAll('.carousel-slide');
-  let slides = Array.prototype.slice.call(slideNodes);
+  let slides = document.querySelectorAll('.carousel-slide');
   let left = document.querySelector('#carousel-btn-left');
   let right = document.querySelector('#carousel-btn-right');
   let indicator = document.querySelector('#carousel-scroll-indicator');
+  let wrapper = document.querySelector('#carousel-wrapper');
 
   // Add bullets to the scoll indicator section for each slide
   for (let i = 0; i < slides.length; i++) {
     let pip = document.createElement('i');
     pip.className = 'carousel-pip fa fa-square';
+    pip.toggle = function () {
+      this.classList.toggle('carousel-pip-active');
+    };
     indicator.appendChild(pip);
   }
   let pips = document.querySelectorAll('.carousel-pip');
-  pips[0].classList.toggle('carousel-pip-active');
 
-  let currentSlideIndex = 0;
-
-  function nextSlide () {
-    if (currentSlideIndex < slides.length - 1) {
-      slides[currentSlideIndex].className = 'carousel-slide carousel-slide-left';
-      slides[currentSlideIndex + 1].className = 'carousel-slide carousel-slide-active';
-      pips[currentSlideIndex].classList.toggle('carousel-pip-active');
-      pips[currentSlideIndex + 1].classList.toggle('carousel-pip-active');
-      currentSlideIndex++;
+  let currentPip = 0;
+  pips[currentPip].toggle();
+  function changePip (direction) {
+    pips[currentPip].toggle();
+    if (direction === 'right') {
+      if (currentPip === slides.length - 1) {
+        currentPip = -1;
+      }
+      currentPip++;
+      pips[currentPip].toggle();
+    } else {
+      if (currentPip === 0) {
+        currentPip = slides.length - 1;
+      }
+      currentPip--;
+      pips[currentPip].toggle();
     }
   }
 
+  function nextSlide () {
+    slides[0].classList.toggle('carousel-slide-left');
+    slides[0].classList.toggle('carousel-slide-active');
+    slides[1].classList.toggle('carousel-slide-active');
+    changePip('right');
+    let forwardTimeout = setTimeout(function () {
+      wrapper.appendChild(slides[0]);
+      slides[0].classList.toggle('carousel-slide-left');
+      slides = document.querySelectorAll('.carousel-slide');
+    }, 600);
+  }
+
   function prevSlide () {
-    if (currentSlideIndex > 0) {
-      slides[currentSlideIndex].className = 'carousel-slide carousel-slide-right';
-      slides[currentSlideIndex - 1].className = 'carousel-slide carousel-slide-active';
-      pips[currentSlideIndex].classList.toggle('carousel-pip-active');
-      pips[currentSlideIndex - 1].classList.toggle('carousel-pip-active');
-      currentSlideIndex--;
-    }
+    wrapper.insertBefore(slides[slides.length - 1], slides[0]);
+    slides = document.querySelectorAll('.carousel-slide');
+    slides[0].classList.toggle('carousel-slide-left');
+    window.getComputedStyle(wrapper);
+    slides[0].classList.toggle('carousel-slide-left');
+    slides[0].classList.toggle('carousel-slide-active');
+    slides[1].classList.toggle('carousel-slide-active');
+    changePip('left');
+
   }
 
   left.addEventListener('click', prevSlide);
