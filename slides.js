7e145e7efae90a65948ef2a58b6964c9208c4d9f
Caleb Pearce <cpearce31@gmail.com>
2017-01-26 11:36:34 -0500

    More styling. Added JS code to append correct number of scroll indicator pips.

--- slides.js
+++ slides.js
@@ -1,1 +1,14 @@
-console.log('hello');
+window.onload = function () {
+  let slideNodes = document.querySelectorAll('.carousel-slide');
+  let slides = Array.prototype.slice.call(slideNodes);
+  let left = document.querySelector('#carousel-btn-left');
+  let right = document.querySelector('#carousel-btn-right');
+  let indicator = document.querySelector('#carousel-scroll-indicator');
+
+  // Add bullets to the scoll indicator section for each slide
+  for (let i = 0; i < slides.length; i++) {
+    let pip = document.createElement('i');
+    pip.className = 'carousel-pip fa fa-square';
+    indicator.appendChild(pip);
+  }
+};
